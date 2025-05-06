243377bb6a6f801fcfc4b152fbc5c9a9f03eefa6
katerinka228 <varfolomeewa.catherine@yandex.ru>
2025-05-06 23:54:01 +0300

    попытка чата заработать в проде

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    include: ['socket.io-client'],
-  },
   plugins: [react()],
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'ws://89.169.168.253:4500',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['socket.io-client']
+  }
 })
-
